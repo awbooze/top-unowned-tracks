26b9b640c28a5966a5028e0b360c87ab9d3d7316
Andrew Booze <42682253+awbooze@users.noreply.github.com>
2020-08-11 22:28:10 -0500

    Get and display 50 top tracks in order of popularity

---
+++ README.md
@@ -0,0 +1,7 @@
+# top_unowned_tracks
+A short little Python script that grabs a user's top tracks from their Spotify account and displays them on the command line. Created to help me decide what music to buy from my Spotify activity. In the future, I may make the time period customizable and look at the file system to see which tracks the user already owns.
+
+## Setup
+* Install spotipy: `pip install spotipy --upgrade`
+* Create a `config.py` file like `example_config.py` in this directory with your Spotify API information and username
+* Run `python main.py`
---
+++ main.py
@@ -0,0 +1,39 @@
+from config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, USERNAME
+import spotipy
+import spotipy.util as util
+
+SCOPE = 'user-top-read'
+
+def get_top_tracks(spotify, time_range):
+    results = spotify.current_user_top_tracks(limit=50, time_range=time_range)
+    results_list = sorted(results['items'], key=lambda item: item['popularity'], 
+            reverse=True)
+    return results_list
+
+def main():
+    token = util.prompt_for_user_token(USERNAME, SCOPE, CLIENT_ID, CLIENT_SECRET, 
+        REDIRECT_URI)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        tracks = get_top_tracks(sp, 'medium_term')
+
+        print('\nTop tracks for user ' + USERNAME + ' for the past six months:\n')
+
+        for item in tracks:
+            artist_string = ''
+            first = True
+            for artist in item['artists']:
+                if first:
+                    first = False
+                    artist_string = artist['name']
+                else:
+                    artist_string = ', '.join([artist_string, artist['name']])
+            
+            print(item['name'] + ' - ' + artist_string + ' - ' 
+                + str(item['popularity']))
+    else:
+        print("Can't get token for", USERNAME)
+
+if __name__ == '__main__':
+    main()
